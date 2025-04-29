0b53a82f8bd209df676a4d002de88fa145fe4ade
Nguyen Duy Hoang <ndhoang22@clc.fitus.edu.vn>
2025-04-29 23:16:36 +0700

    feat: add new icon.svg and remove vite.svg; update styles and theme in index.css and root.tsx

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StyleProvider } from "@ant-design/cssinjs";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, Spin } from "antd";
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -32,11 +32,47 @@ const queryClient = new QueryClient({
   },
 });
 
+export function HydrateFallback() {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#c99383",
+        },
+      }}
+    >
+      <div className="relative min-h-screen">
+        <Spin
+          size="large"
+          className="absolute top-1/3 left-1/2 -translate-x-1/2"
+        />
+      </div>
+    </ConfigProvider>
+  );
+}
+
 export default function Root() {
   return (
     <QueryClientProvider client={queryClient}>
       <StyleProvider layer>
-        <ConfigProvider>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorSuccess: "#a3b18a",
+              colorError: "#bf1922",
+              colorWarning: "#e1ad01",
+              colorPrimary: "#c99383",
+              colorInfo: "#c99383",
+              colorTextBase: "#112d4e",
+              colorBgBase: "#f7e8d3",
+            },
+            components: {
+              Rate: {
+                starColor: "rgb(255,149,41)",
+              },
+            },
+          }}
+        >
           <Outlet />
           <ReactQueryDevtools initialIsOpen={false} />
         </ConfigProvider>
