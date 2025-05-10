05c0e420707dbf4828c49ee350a767967b9784c8
quocnghia32 <nguyennghia3224@gmail.com>
2025-05-11 00:33:19 +0700

    Delete TODOS

--- backend/internal/repository/repository.go
+++ backend/internal/repository/repository.go
@@ -11,11 +11,6 @@ import (
 )
 
 type Repository interface {
-	Create(todo *model.Todo) error
-	FindAll() ([]model.Todo, error)
-	FindByID(id uint) (*model.Todo, error)
-	Update(todo *model.Todo) error
-	Delete(id uint) error
 	FindRestaurantByID(id string, lat float64, lng float64) (*model.Restaurant, error)
 	FindAllFoodTypes() ([]string, error)
 	FindDishesByRestaurantID(id string) ([]model.Dish, error)
@@ -33,58 +28,6 @@ func NewRepository(db *sql.DB) Repository {
 	return &repository{db: db}
 }
 
-func (r *repository) Create(todo *model.Todo) error {
-	query := "INSERT INTO todos (title, description, status, created_at, updated_at) VALUES (?, ?, ?, NOW(), NOW())"
-	_, err := r.db.Exec(query, todo.Title, todo.Description, todo.Status)
-	return err
-}
-
-func (r *repository) FindAll() ([]model.Todo, error) {
-	query := "SELECT id, title, description, status, created_at, updated_at FROM todos"
-	rows, err := r.db.Query(query)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var todos []model.Todo
-	for rows.Next() {
-		var todo model.Todo
-		if err := rows.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Status, &todo.CreatedAt, &todo.UpdatedAt); err != nil {
-			return nil, err
-		}
-		todos = append(todos, todo)
-	}
-
-	return todos, nil
-}
-
-func (r *repository) FindByID(id uint) (*model.Todo, error) {
-	query := "SELECT id, title, description, status, created_at, updated_at FROM todos WHERE id = ?"
-	row := r.db.QueryRow(query, id)
-
-	var todo model.Todo
-	if err := row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Status, &todo.CreatedAt, &todo.UpdatedAt); err != nil {
-		if err == sql.ErrNoRows {
-			return nil, errors.New("not found")
-		}
-		return nil, err
-	}
-
-	return &todo, nil
-}
-
-func (r *repository) Update(todo *model.Todo) error {
-	query := "UPDATE todos SET title = ?, description = ?, status = ?, updated_at = NOW() WHERE id = ?"
-	_, err := r.db.Exec(query, todo.Title, todo.Description, todo.Status, todo.ID)
-	return err
-}
-
-func (r *repository) Delete(id uint) error {
-	query := "DELETE FROM todos WHERE id = ?"
-	_, err := r.db.Exec(query, id)
-	return err
-}
 
 // Haversine function to calculate distance between two points on the Earth
 func haversine(lat1, lon1, lat2, lon2 float64) float64 {
